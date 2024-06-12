76189984929abac073ca6c49ab578ef85d7f9f8a
shun <zhu1185056939@gmail.com>
2024-06-12 22:20:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,3 @@
 
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate

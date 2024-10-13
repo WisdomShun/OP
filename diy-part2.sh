8911bba5aa458524341bd71f07b36aac4fa34c2b
shun <zhu1185056939@gmail.com>
2024-10-13 09:24:43 +0800

    upgrade go version to 23.x

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,4 +20,4 @@ sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_gener
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang

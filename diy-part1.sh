290540d4bbd6502ccc307c410e079ffd0c344b14
shun <zhu1185056939@gmail.com>
2025-03-16 19:18:06 +0800

    add xiaorouji repo to try for fixing build issue

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,6 @@
 
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '3i src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
+
+
